0424da61369559b5b5415588b9c8bb7e956795f8
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2023-12-05 10:47:14 -0300

    Update QuestaoMultiplaEscolha.java

--- BancoQuestoes/QuestaoMultiplaEscolha.java
+++ BancoQuestoes/QuestaoMultiplaEscolha.java
@@ -23,4 +23,27 @@ public class QuestaoMultiplaEscolha extends QuestaoObjetiva{
 		
 		return this.notaObtida = avaliacao;
 	}
+	public String mostrarQuestaoSemResposta() {
+		return ("*** Questão de Múltipla Escolha ***"
+				+ "\nNúmero     : " + this.getIdQuestao()
+				+ "\nEnunciado  : " + this.getEnunciado()
+				+ "\n\nA - " + this.getItemA()
+				+ "\nB - " + this.getItemB()
+				+ "\nC - " + this.getItemC()
+				+ "\n\nNota da questão  : " + this.getNotaPadrao() 
+				);
+	}
+	@Override
+	public String toString() {
+		
+		
+		return ("*** Questão de Múltipla Escolha ***"
+				+ "\nNúmero     : " + this.getIdQuestao()
+				+ "\nEnunciado  : " + this.getEnunciado()
+				+ "\n\nA - " + this.getItemA() + (this.itemCorreto == 'A' ? " Correta" : "")
+				+ "\nB - " + this.getItemB() + (this.itemCorreto == 'B' ? " Correta" : "")
+				+ "\nC - " + this.getItemC() + (this.itemCorreto == 'C' ? " Correta" : "")
+				+ "\n\nNota da questão  : " + this.getNotaPadrao() 
+				);
+	}
 }
