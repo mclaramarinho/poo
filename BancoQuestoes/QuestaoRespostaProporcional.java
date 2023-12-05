903a85305d2a910bcde4195aaa3d04fbcc5e7600
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2023-12-05 10:48:03 -0300

    Update QuestaoRespostaProporcional.java

--- BancoQuestoes/QuestaoRespostaProporcional.java
+++ BancoQuestoes/QuestaoRespostaProporcional.java
@@ -4,10 +4,19 @@ public class QuestaoRespostaProporcional extends QuestaoObjetiva{
 	private int percentItemA, percentItemB, percentItemC;
 	private char itemSelecionado;
 	
-	public QuestaoRespostaProporcional(String enunciado, String itemA, String itemB, String itemC, double notaPadrao, int percentItemA, int percentItemB, int percentItemC) {
+	public QuestaoRespostaProporcional(String enunciado, String itemA, String itemB, String itemC, double notaPadrao, int percentItemA, int percentItemB, int percentItemC) throws AnswerDoesNotExist{
 		super(enunciado, itemA, itemB, itemC, notaPadrao);
+		if(percentItemA != 25 && percentItemA != 50 && percentItemA != 100) {
+			throw new AnswerDoesNotExist();
+		}
 		this.percentItemA = percentItemA;
+		if(percentItemB != 25 && percentItemB != 50 && percentItemB != 100) {
+			throw new AnswerDoesNotExist();
+		}
 		this.percentItemB = percentItemB;
+		if(percentItemC != 25 && percentItemC != 50 && percentItemC != 100) {
+			throw new AnswerDoesNotExist();
+		}
 		this.percentItemC = percentItemC;
 	}
 	
@@ -30,7 +39,30 @@ public class QuestaoRespostaProporcional extends QuestaoObjetiva{
 			break;
 		}
 		
-		return this.notaObtida = avaliacao;
+		return this.notaObtida = Math.round(avaliacao * 100.0) / 100.0;
 	}
 	
+	public String mostrarQuestaoSemResposta() {
+		return("*** Questão de Resposta Proporcional ***"
+				+ "\nNúmero: " + this.getIdQuestao()
+				+ "\nEnunciado: " + this.getEnunciado()
+				+ "\n\nA - " + this.getItemA()
+				+ "\nB - " + this.getItemB()
+				+ "\nC - " + this.getItemC()
+				+ "\n\nNota da questão: " + this.getNotaPadrao()
+				);
+	}
+	
+	@Override
+	public String toString() {
+		return("*** Questão de Resposta Proporcional ***"
+				+ "\nNúmero: " + this.getIdQuestao()
+				+ "\nEnunciado: " + this.getEnunciado()
+				+ "\n\nA - " + this.getItemA() + " " + this.percentItemA + "%"
+				+ "\nB - " + this.getItemB() + " " + this.percentItemB + "%"
+				+ "\nC - " + this.getItemC() + " " + this.percentItemC + "%"
+				+ "\n\nNota da questão: " + this.getNotaPadrao()
+				);
+				
+	}
 }
